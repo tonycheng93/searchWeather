4ffd9a01da115e4aec031f164b2f36c43e548219
tonycheng93 <bc1341286335@gmail.com>
2015-08-26 12:00:44 +0800

    添加注释

--- src/com/example/jsondemo/Utils/ParseJSON.java
+++ src/com/example/jsondemo/Utils/ParseJSON.java
@@ -28,6 +28,10 @@ public class ParseJSON {
 		}
 		/**
 		 * 增加对retData是否为空的判断，避免出现输入地区不存在时，出现空指针异常
+		 * 这里有一个很奇怪的问题就是，JSON设计的格式，
+		 * 1、当返回正确结果时，retData是一个对象
+		 * 2、当返回错误的结果时，retData是一个数组
+		 * 如果不对这种情况事先加以判断就会出现空指针异常的现象
 		 */
 		JSONObject retData = json.optJSONObject("retData");
 		if (retData != null) {
